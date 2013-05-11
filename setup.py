d8c028a0ec923ebb11608e43979dc6623f6a0054
Alex Stapleton <alexs@prol.etari.at>
2013-05-11 16:57:57 +0100

    Fleshed out the setup file a bit

--- setup.py
+++ setup.py
@@ -1,8 +1,27 @@
 from setuptools import setup
 
 setup(
-    name='Duvet Cover',
+    name='python-duvet',
+    version='0.1',
     packages=['duvet'],
+
+    author='Alex Stapleton',
+    author_email='alexs@prol.etari.at',
+    description="""
+    A nose plugin that does various things with coverage data.
+
+    Currently it only attempts to detect which tests are effected by your
+    changes in git and then re-orders or skips tests that haven't changed.
+    """,
+    license='LGPLv3',
+    url='https://github.com/public/python-duvet',
+
+    install_requires=[
+        "GitPython",
+        "bzr",
+        "nose",
+        "coverage"
+    ],
 
     entry_points={
         'nose.plugins.0.10': [
